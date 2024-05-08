e425026ce1ae2193e016747d9664949841c8b812
talhasheikh041 <talhasheikh041@gmail.com>
2024-05-08 19:39:41 +0500

    added pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,14 @@
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom'
+
+// Pages
+import { Customers } from '@/pages'
+import { Dashboard } from '@/pages'
+import { Products } from '@/pages'
+import { Transactions } from '@/pages'
+
 function App() {
+   const router = createBrowserRouter(createRoutesFromElements(<Route></Route>))
+
    return <>Hello World</>
 }
 
---
+++ src/pages/index.tsx
@@ -0,0 +1,4 @@
+export * as Customers from './Customers'
+export * as Dashboard from './Dashboard'
+export * as Products from './Products'
+export * as Transactions from './Transactions'
