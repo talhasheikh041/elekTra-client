48bed2917a9684a92df77811d9452503b910582e
talhasheikh041 <talhasheikh041@gmail.com>
2024-05-10 19:19:37 +0500

    Sidebar complete

--- src/features/components/layout/Root-Layout.tsx
+++ src/features/components/layout/Root-Layout.tsx
@@ -3,12 +3,14 @@ import { Outlet } from 'react-router-dom'
 
 const RootLayout = () => {
    return (
-      <>
-         <Sidebar />
-         <main>
+      <div className='flex'>
+         <aside className='min-w-72 absolute sm:relative -translate-x-full sm:translate-x-0 transition-transform left-0 duration-300'>
+            <Sidebar />
+         </aside>
+         <main className='flex-auto bg-gray-50'>
             <Outlet />
          </main>
-      </>
+      </div>
    )
 }
 
